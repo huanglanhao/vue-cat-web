d75ed701f97edc46908d80e0e3b21137a8cb80a9
huanglanhao <1351341632@qq.com>
2020-09-29 11:11:34 +0800

    我的好友
    Signed-off-by: huanglanhao <1351341632@qq.com>

---
+++ src/pages/my-home/friends/index.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="friends">
+    <div calss="friends-title">
+        <div class="el-icon-arrow-left"></div>
+        <div>我的好友</div>
+    </div>
+</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import {
+  Button
+} from 'element-ui'
+Vue.use(Button)
+
+export default {
+  name: 'friends',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  mounted: {
+    add () {
+      console.log(12)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.friends {
+    width: 23.4375rem;
+    height: 23.4375rem;
+    background-color: #fff;
+}
+
+.friends-title {
+    width: 100%;
+    line-height: 80px;
+    background: red;
+    display: inline-flex;
+    justify-items: flex-end;
+}
+
+span {
+    flex: 1
+}
+</style>
